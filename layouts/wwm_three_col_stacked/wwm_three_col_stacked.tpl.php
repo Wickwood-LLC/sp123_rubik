090b05ea901763d69aa40c852e363815a38b6073
palagpatski <palagpatski@gmail.com>
2015-06-30 14:16:32 +0800

    Remove debugging

--- layouts/wwm_three_col_stacked/wwm_three_col_stacked.tpl.php
+++ layouts/wwm_three_col_stacked/wwm_three_col_stacked.tpl.php
@@ -17,7 +17,6 @@
  *   $content['right_sidebar']
  */
 ?>
-<?php dpm($content); ?>
 <div class="panel-display panel-wwm-3col-stacked 
   <?php if ($content['left_sidebar'] && $content['right_sidebar']) { 
     print "has-two-sidebars";
