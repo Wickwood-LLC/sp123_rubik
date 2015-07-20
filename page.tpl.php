07fb297d2c42e257f2a714db8a023c3b3f8ab5e6
palagpatski <palagpatski@gmail.com>
2015-07-20 12:48:44 +0800

    Added logo, site name, and slogan to the template.

--- page.tpl.php
+++ page.tpl.php
@@ -1,5 +1,16 @@
-<?php print render($page['header']); ?>
-<?php print render($page['navbar']); ?>
+<header>
+  <a href="/" rel="home" id="logo">
+    <img src="<?php print $logo; ?>" />
+  </a>
+  <h1 id="site-name">
+    <?php print $site_name; ?>
+  </h1>
+  <h2 id="site-slogan">
+    <?php print $site_slogan; ?>
+  </h2>
+  <?php print render($page['header']); ?>
+  <?php print render($page['navbar']); ?>
+</header>
 <?php if (theme_get_setting('rubik_show_branding')): ?>
 <div id='branding'><div class='limiter clearfix'>
   <div class='breadcrumb clearfix'><?php print $breadcrumb ?></div>
