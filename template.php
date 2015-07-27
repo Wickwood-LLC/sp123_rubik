431753f2f55b7ed45a92faf468b67934b00350cc
palagpatski <palagpatski@gmail.com>
2015-07-27 15:38:52 +0800

    Override breadcrumb adding current page title to the end of the trail.

---
+++ template.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Override of theme('breadcrumb').
+ */
+function full_rubik_breadcrumb($vars) {
+  $output = '';
+
+  // // Add current page onto the end.
+  // if (!drupal_is_front_page()) {
+  //   $item = menu_get_item();
+  //   $end = end($vars['breadcrumb']);
+  //   if ($end && strip_tags($end) !== $item['title']) {
+  //     $vars['breadcrumb'][] = (isset($item['localized_options']['html']) && $item['localized_options']['html']) ? $item['title'] : check_plain($item['title']);
+  //   }
+  // }
+
+  // Optional: Add the site name to the front of the stack.
+  if (!empty($vars['prepend'])) {
+    $site_name = empty($vars['breadcrumb']) ? "<strong>". check_plain(variable_get('site_name', '')) ."</strong>" : l(variable_get('site_name', ''), '<front>', array('purl' => array('disabled' => TRUE)));
+    array_unshift($vars['breadcrumb'], $site_name);
+  }
+
+  $depth = 0;
+  foreach ($vars['breadcrumb'] as $link) {
+
+    // If the item isn't a link, surround it with a strong tag to format it like
+    // one.
+    if (!preg_match('/^<a/', $link) && !preg_match('/^<strong/', $link)) {
+      $link = '<strong>' . $link . '</strong>';
+    }
+
+    $output .= "<span class='breadcrumb-link breadcrumb-depth-{$depth}'>{$link}</span>";
+    $depth++;
+  }
+  return $output;
+}
