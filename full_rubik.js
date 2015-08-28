808d3aa20e04b8570b4bca0db0b3865e0f220639
palagpatski <palagpatski@gmail.com>
2015-08-28 12:50:09 +0800

    Remove title attribute and instead add data-title attribute.

--- full_rubik.js
+++ full_rubik.js
@@ -1,7 +1,11 @@
 (function ($) {
   Drupal.behaviors.hideTooltip = {
     attach: function (context, settings) {
-    	$('.cke_toolbar a.cke_button').data('title',$this.attr('title')).removeAttr('title');
+    	$('.cke_toolbar a.cke_button').each( function() {
+		    var $this = $(this);
+		    $this.data('title',$this.attr('title'));
+		    $this.removeAttr('title');
+		});
     }
   };
 }(jQuery));
