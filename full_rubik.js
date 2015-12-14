5935f570dfec6e81d6b27661792d2732ee072fe2
palagpatski <palagpatski@gmail.com>
2015-12-14 19:43:04 +0800

    Change trigger event to apply only to the element.

--- full_rubik.js
+++ full_rubik.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.collapseBlock = {
     attach: function (context, settings) {
-    	$('.panelizer-view-mode.node-embedded-to-profile>h3').click(function() {
+    	$('.panelizer-view-mode.node-embedded-to-profile>h3').focus(function() {
     		$('.panelizer-view-mode.node-embedded-to-profile>.panel-display').slideToggle();
 		});
     }
