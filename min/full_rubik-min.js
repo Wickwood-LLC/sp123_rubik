49f04b4a7d7ccf2b45b51edb6a0385f891f13f41
palagpatski <palagpatski@gmail.com>
2015-12-14 18:52:20 +0800

    Improve selectors

--- min/full_rubik-min.js
+++ min/full_rubik-min.js
@@ -1,1 +1,1 @@
-!function($){Drupal.behaviors.collapseBlock={attach:function(e,o){$(".panelizer-view-mode.node-embedded-to-profile>h3").click(function(){$(".panelizer-view-mode.node-embedded-to-profile>.panel-display").toggle()})}}}(jQuery);
+!function($){Drupal.behaviors.collapseBlock={attach:function(e,o){$(".panelizer-view-mode.node-embedded-to-profile h3").click(function(){$(".panelizer-view-mode.node-embedded-to-profile .panel-display").toggle()})}}}(jQuery);
