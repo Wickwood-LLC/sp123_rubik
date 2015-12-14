1c49b5f31524cd9005847d9f1a63ffa93086eec0
palagpatski <palagpatski@gmail.com>
2015-12-14 18:57:50 +0800

    Change toggle style.

--- full_rubik.js
+++ full_rubik.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.collapseBlock = {
     attach: function (context, settings) {
     	$('.panelizer-view-mode.node-embedded-to-profile>h3').click(function() {
-    		$('.panelizer-view-mode.node-embedded-to-profile>.panel-display').toggle();
+    		$('.panelizer-view-mode.node-embedded-to-profile>.panel-display').slideToggle();
 		});
     }
   };
