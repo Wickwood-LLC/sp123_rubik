18e74fba678a3d1dda97ef66ce531f39d3b70096
palagpatski <palagpatski@gmail.com>
2015-12-14 18:49:55 +0800

    Improve selectors

--- full_rubik.js
+++ full_rubik.js
@@ -1,8 +1,8 @@
 (function ($) {
   Drupal.behaviors.collapseBlock = {
     attach: function (context, settings) {
-    	$('.panelizer-view-mode.node-embedded-to-profile > h3').click(function() {
-    		$('.panelizer-view-mode.node-embedded-to-profile > .panel-display').toggle();
+    	$('.panelizer-view-mode.node-embedded-to-profile>h3').click(function() {
+    		$('.panelizer-view-mode.node-embedded-to-profile>.panel-display').toggle();
 		});
     }
   };
