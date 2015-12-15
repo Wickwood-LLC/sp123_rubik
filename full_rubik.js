6fea40b868136cdd3cb2d9f05c7de7c7c79e907d
palagpatski <palagpatski@gmail.com>
2015-12-15 12:55:46 +0800

    Added easing

--- full_rubik.js
+++ full_rubik.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
     	$('.panelizer-view-mode.node-embedded-to-profile>h3').on("click", function() {
     		var $this = $(this);
-    		$this.parents('.panelizer-view-mode.node-embedded-to-profile').find('.panel-display').first().slideToggle(400, "easeInOutCubic");
+    		$this.parents('.panelizer-view-mode.node-embedded-to-profile').find('.panel-display').first().slideToggle('fast', 'easeInOutCubic');
 		});
     }
   };
