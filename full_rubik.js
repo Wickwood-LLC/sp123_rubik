4479883362929234fbc7d973a709707866be5082
palagpatski <palagpatski@gmail.com>
2015-12-15 11:35:03 +0800

    Select only the first one that matches

--- full_rubik.js
+++ full_rubik.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
     	$('.panelizer-view-mode.node-embedded-to-profile>h3').click(function() {
     		var $this = $(this);
-    		$this.parents('.panelizer-view-mode.node-embedded-to-profile').find('.panel-display:first-child').slideToggle();
+    		$this.parents('.panelizer-view-mode.node-embedded-to-profile').find('.panel-display').first().slideToggle();
 		});
     }
   };
