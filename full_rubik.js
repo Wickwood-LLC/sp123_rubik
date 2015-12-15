56ecd3e5864509cc7698c47c6066fd3721ddda25
palagpatski <palagpatski@gmail.com>
2015-12-15 10:41:09 +0800

    Add find method

--- full_rubik.js
+++ full_rubik.js
@@ -1,8 +1,8 @@
 (function ($) {
   Drupal.behaviors.collapseBlock = {
     attach: function (context, settings) {
-    	$('.panelizer-view-mode.node-embedded-to-profile>h3').click(function() {
-    		$('.panelizer-view-mode.node-embedded-to-profile>.panel-display').slideToggle();
+    	$('.panelizer-view-mode.node-embedded-to-profile').find('h3').click(function() {
+    		$(this).find('.panel-display').slideToggle();
 		});
     }
   };
