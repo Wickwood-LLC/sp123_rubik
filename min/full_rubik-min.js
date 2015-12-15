526b0e40ca5800fde4fab660523c3cab530c8e2b
palagpatski <palagpatski@gmail.com>
2015-12-15 11:33:08 +0800

    Select only the first one that matches

--- min/full_rubik-min.js
+++ min/full_rubik-min.js
@@ -1,1 +1,1 @@
-!function($){Drupal.behaviors.collapseBlock={attach:function(e,i){$(".panelizer-view-mode.node-embedded-to-profile>h3").click(function(){var e=$(this);e.parents(".panelizer-view-mode.node-embedded-to-profile").find(".panel-display").slideToggle()})}}}(jQuery);
+!function($){Drupal.behaviors.collapseBlock={attach:function(e,i){$(".panelizer-view-mode.node-embedded-to-profile>h3").click(function(){var e=$(this);e.parents(".panelizer-view-mode.node-embedded-to-profile").find(".panel-display:first-child").slideToggle()})}}}(jQuery);
