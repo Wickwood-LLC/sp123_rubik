27a51a509d39fdbb7cd792856a51134f50164451
palagpatski <palagpatski@gmail.com>
2015-12-15 12:57:37 +0800

    Remove easing.

--- min/full_rubik-min.js
+++ min/full_rubik-min.js
@@ -1,1 +1,1 @@
-!function($){Drupal.behaviors.collapseBlock={attach:function(e,i){$(".panelizer-view-mode.node-embedded-to-profile>h3").on("click",function(){var e=$(this);e.parents(".panelizer-view-mode.node-embedded-to-profile").find(".panel-display").first().slideToggle("fast","easeInOutCubic")})}}}(jQuery);
+!function($){Drupal.behaviors.collapseBlock={attach:function(e,i){$(".panelizer-view-mode.node-embedded-to-profile>h3").on("click",function(){var e=$(this);e.parents(".panelizer-view-mode.node-embedded-to-profile").find(".panel-display").first().slideToggle()})}}}(jQuery);
