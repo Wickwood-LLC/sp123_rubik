a84ccdce8d8c058c5893da3212d1aa9f4184506f
palagpatski <palagpatski@gmail.com>
2015-12-15 18:49:16 +0800

    Added off event.

--- full_rubik.js
+++ full_rubik.js
@@ -1,9 +1,10 @@
 (function ($) {
   Drupal.behaviors.collapseBlock = {
     attach: function (context, settings) {
+    	$('.panelizer-view-mode.node-embedded-to-profile>h3').off("click");
     	$('.panelizer-view-mode.node-embedded-to-profile>h3').on("click", function() {
     		var $this = $(this);
-    		$this.parents('.panelizer-view-mode.node-embedded-to-profile').find('.panel-display').first().slideToggle();
+    		$this.parents('.panelizer-view-mode.node-embedded-to-profile').find('.panel-display').first().slideToggle(1000);
 		});
     }
   };
