a6a91c003d8e9e921d7e4db6631c811163b8cbfb
palagpatski <palagpatski@gmail.com>
2016-01-15 23:07:35 +0800

    Dpm loan

--- template.php
+++ template.php
@@ -256,6 +256,7 @@ function full_rubik_form_alter(&$form, $form_state, $form_id) {
 
     $entities = $form['profile_bwma_client']['field_loans']['und']['entities'];
     foreach ($entities as $loan) {
+      dpm($loan);
       if(is_int($loan)) {
         $loan['form']['field_deferred_enrollment_fees']['und']['add_more']['#value'] = 'Add Another Deferred Fee';  // Override 'Enrolment Fees' add buttton text
         foreach ($loan['form']['field_deferred_enrollment_fees']['und'] as $deferred_fee) {
@@ -266,7 +267,6 @@ function full_rubik_form_alter(&$form, $form_state, $form_id) {
       }
     }
   }
-  dpm($form);
 }
 
 //Disable sticky headers
