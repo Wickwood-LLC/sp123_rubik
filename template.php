ede6b10046ab57122ff109e4022619d995d472dd
palagpatski <palagpatski@gmail.com>
2016-01-28 23:33:10 +0800

    dpm header variables

--- template.php
+++ template.php
@@ -291,6 +291,8 @@ function full_rubik_table($variables) {
 
     unset($variables['header'][3]);
 
+    dpm($variables);
+
     // // Add sticky headers, if applicable.
     // if (count($header) && $sticky) {
     //   drupal_add_js('misc/tableheader.js');
