b170105476028e2739ef53049ddd5ff34ef931c9
palagpatski <palagpatski@gmail.com>
2016-02-09 14:11:19 +0800

    Add resize event to recalculate variables.

--- full_rubik.js
+++ full_rubik.js
@@ -14,31 +14,33 @@
     attach: function (context, settings) {
       if (!!$('.pane-profile2-form-buttons').offset()) {  // make sure ".pane-profile2-form-buttons" element exists
 
-        $('.pane-profile2-form-buttons').css('position','static');  // this is to reset the position of the element whenever the page is update with AJAX.
+        $(window).on("load resize",function() {           // fire the script on load and resize
+          $('.pane-profile2-form-buttons').css('position','static');  // this is to reset the position of the element whenever the page is updated with AJAX.
 
-        var stickyTop = $('.pane-profile2-form-buttons').offset().top;
-        var windowHeight = $(window).height();
-        var buttonWidth = $('.pane-profile2-form-buttons').width();
-        var windowTop = $(window).scrollTop(); // returns number  
-        var currentPosition = windowTop + windowHeight;
-
-        if (stickyTop > currentPosition) {
-          $('.pane-profile2-form-buttons').css({ position: 'fixed', bottom: 0, width: buttonWidth });
-        }
-        else {
-          $('.pane-profile2-form-buttons').css('position','static');
-        }
-  
-        $(window).scroll(function(){ // scroll event 
+          var stickyTop = $('.pane-profile2-form-buttons').offset().top;
+          var windowHeight = $(window).height();
+          var buttonWidth = $('.pane-profile2-form-buttons').width();
           var windowTop = $(window).scrollTop(); // returns number  
           var currentPosition = windowTop + windowHeight;
-  
+
           if (stickyTop > currentPosition) {
             $('.pane-profile2-form-buttons').css({ position: 'fixed', bottom: 0, width: buttonWidth });
           }
           else {
             $('.pane-profile2-form-buttons').css('position','static');
           }
+    
+          $(window).scroll(function(){ // scroll event 
+            var windowTop = $(window).scrollTop(); // returns number  
+            var currentPosition = windowTop + windowHeight;
+    
+            if (stickyTop > currentPosition) {
+              $('.pane-profile2-form-buttons').css({ position: 'fixed', bottom: 0, width: buttonWidth });
+            }
+            else {
+              $('.pane-profile2-form-buttons').css('position','static');
+            }
+          });
         });
       }
     }
