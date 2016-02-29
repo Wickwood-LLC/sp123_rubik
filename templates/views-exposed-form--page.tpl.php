ebe65ed76b5d6833027b3ec0a2c91a0d36b55152
palagpatski <palagpatski@gmail.com>
2016-02-29 16:49:10 +0800

    Hide secondary filter in the first instance

--- templates/views-exposed-form--page.tpl.php
+++ templates/views-exposed-form--page.tpl.php
@@ -33,56 +33,60 @@
     <span class="fieldset-title fieldset-legend">Sorting and Filtering</span>
   </legend>
   <div class="views-exposed-widgets clearfix">
-    <?php foreach ($widgets as $id => $widget): ?>
-      <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
-        <?php if (!empty($widget->label)): ?>
-          <label for="<?php print $widget->id; ?>">
-            <?php print $widget->label; ?>
-          </label>
-        <?php endif; ?>
-        <?php if (!empty($widget->operator)): ?>
-          <div class="views-operator">
-            <?php print $widget->operator; ?>
+    <div class="basic-options">
+      <?php foreach ($widgets as $id => $widget): ?>
+        <?php if !($widget['filter-secondary']): ?>
+          <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
+            <?php if (!empty($widget->label)): ?>
+              <label for="<?php print $widget->id; ?>">
+                <?php print $widget->label; ?>
+              </label>
+            <?php endif; ?>
+            <?php if (!empty($widget->operator)): ?>
+              <div class="views-operator">
+                <?php print $widget->operator; ?>
+              </div>
+            <?php endif; ?>
+            <div class="views-widget">
+              <?php print $widget->widget; ?>
+            </div>
+            <?php if (!empty($widget->description)): ?>
+              <div class="description">
+                <?php print $widget->description; ?>
+              </div>
+            <?php endif; ?>
           </div>
         <?php endif; ?>
-        <div class="views-widget">
-          <?php print $widget->widget; ?>
+      <?php endforeach; ?>
+      <?php if (!empty($sort_by)): ?>
+        <div class="views-exposed-widget views-widget-sort-by">
+          <?php print $sort_by; ?>
         </div>
-        <?php if (!empty($widget->description)): ?>
-          <div class="description">
-            <?php print $widget->description; ?>
-          </div>
-        <?php endif; ?>
-      </div>
-    <?php endforeach; ?>
-    <?php if (!empty($sort_by)): ?>
-      <div class="views-exposed-widget views-widget-sort-by">
-        <?php print $sort_by; ?>
-      </div>
-      <div class="views-exposed-widget views-widget-sort-order">
-        <?php print $sort_order; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($items_per_page)): ?>
-      <div class="views-exposed-widget views-widget-per-page">
-        <?php print $items_per_page; ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($offset)): ?>
-      <div class="views-exposed-widget views-widget-offset">
-        <?php print $offset; ?>
-      </div>
-    <?php endif; ?>
-    <div class="actions">
-      <div class="views-exposed-widget views-submit-button">
-        <?php print $button; ?>
-      </div>
-      <?php if (!empty($reset_button)): ?>
-        <div class="views-exposed-widget views-reset-button">
-          <?php print $reset_button; ?>
+        <div class="views-exposed-widget views-widget-sort-order">
+          <?php print $sort_order; ?>
         </div>
       <?php endif; ?>
-    </div>  
+      <?php if (!empty($items_per_page)): ?>
+        <div class="views-exposed-widget views-widget-per-page">
+          <?php print $items_per_page; ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($offset)): ?>
+        <div class="views-exposed-widget views-widget-offset">
+          <?php print $offset; ?>
+        </div>
+      <?php endif; ?>
+      <div class="actions">
+        <div class="views-exposed-widget views-submit-button">
+          <?php print $button; ?>
+        </div>
+        <?php if (!empty($reset_button)): ?>
+          <div class="views-exposed-widget views-reset-button">
+            <?php print $reset_button; ?>
+          </div>
+        <?php endif; ?>
+      </div>  
+    </div>
     <div class="advanced-options">
       <?php print $widgets['filter-secondary']->widget; ?>
     </div>
