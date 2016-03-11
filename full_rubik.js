e9d64ea75a7f4598720ef2c442bb0ade3c48d52a
palagpatski <palagpatski@gmail.com>
2016-03-11 14:48:20 +0800

    Replace with "this" variable

--- full_rubik.js
+++ full_rubik.js
@@ -12,45 +12,45 @@
 
   Drupal.behaviors.stickyButtons = {
     attach: function (context, settings) {
-      if (!!$('div[id*="edit-actions"]').offset()) {  // make sure "div[id*="edit-actions"]" element exists
 
-        $(window).on("load resize",function() {           // fire the script on load and resize
+      $(window).find('div[id*="edit-actions"]').each(function() {           // fire the script on load and resize
 
-          var stickyTop = $('div[id*="edit-actions"]').offset().top;
-          var windowHeight = $(window).height();
-          var buttonWidth = $('div[id*="edit-actions"]').parent().width();
+        var $this = $(this);
+      
+        var stickyTop = $this.offset().top;
+        var windowHeight = $(window).height();
+        var buttonWidth = $('div[id*="edit-actions"]').parent().width();
+        var windowTop = $(window).scrollTop(); // returns number  
+        var currentPosition = windowTop + windowHeight;
+
+        $('div[id*="edit-actions"]').css('position','static');  // this is to reset the position of the element whenever the page is updated with AJAX.
+        $('div[id*="edit-actions"]').width(buttonWidth);      // reset button width
+
+        if (stickyTop > currentPosition) {
+          $('div[id*="edit-actions"]').css({ position: 'fixed', top: 'initial', bottom: 0, width: $(this).parent().width() });
+        }
+        else if ((stickyTop - windowTop) < 0) {
+          $('div[id*="edit-actions"]').css({ position: 'fixed', top: 0, bottom: 'initial', width: $(this).parent().width() });
+        }
+        else {
+          $('div[id*="edit-actions"]').css('position','static');
+        }
+  
+        $(window).scroll(function(){ // scroll event 
           var windowTop = $(window).scrollTop(); // returns number  
           var currentPosition = windowTop + windowHeight;
-
-          $('div[id*="edit-actions"]').css('position','static');  // this is to reset the position of the element whenever the page is updated with AJAX.
-          $('div[id*="edit-actions"]').width(buttonWidth);      // reset button width
-
+  
           if (stickyTop > currentPosition) {
-            $('div[id*="edit-actions"]').css({ position: 'fixed', top: 'initial', bottom: 0, width: $(this).parent().width() });
+            $('div[id*="edit-actions"]').css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth });
           }
           else if ((stickyTop - windowTop) < 0) {
-            $('div[id*="edit-actions"]').css({ position: 'fixed', top: 0, bottom: 'initial', width: $(this).parent().width() });
+            $('div[id*="edit-actions"]').css({ position: 'fixed', top: 0, bottom: 'initial', width: buttonWidth });
           }
           else {
             $('div[id*="edit-actions"]').css('position','static');
           }
-    
-          $(window).scroll(function(){ // scroll event 
-            var windowTop = $(window).scrollTop(); // returns number  
-            var currentPosition = windowTop + windowHeight;
-    
-            if (stickyTop > currentPosition) {
-              $('div[id*="edit-actions"]').css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth });
-            }
-            else if ((stickyTop - windowTop) < 0) {
-              $('div[id*="edit-actions"]').css({ position: 'fixed', top: 0, bottom: 'initial', width: buttonWidth });
-            }
-            else {
-              $('div[id*="edit-actions"]').css('position','static');
-            }
-          });
         });
-      }
+      });
     }
   };
 }(jQuery));
