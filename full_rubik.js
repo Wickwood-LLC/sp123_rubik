9d856f48e81298e541447b1ba81b3da3f99c58dd
palagpatski <palagpatski@gmail.com>
2016-03-23 11:28:10 +0800

    Improvements to the code.

--- full_rubik.js
+++ full_rubik.js
@@ -13,25 +13,39 @@
   Drupal.behaviors.stickyButtons = {
     attach: function (context, settings) {
 
-      $( window ).on("load resize", function() {
+      function reset() {
+        $('div[id*="edit-actions"]').each(function (e) {
+          $this = $(this);
+          $this.width( $this.parent().width() );
+          $this.parent().height( $this.outerHeight() );
+          $this.css('position','static');
+        });
+      }
+      reset();
+      $(window).resize
 
+      var stickyTop;
+      var windowHeight;
+      var buttonWidth;
+      var buttonHeight;
+      var windowTop;
+      var currentPosition;
+      var $this;
+
+      $(window).on("load resize", function() {
         $('html').find('div[id*="edit-actions"]').each(function() {
+          $this = $('div[id*="edit-actions"]');
 
-          var $this = $('div[id*="edit-actions"]');
-
-          var stickyTop = $this.offset().top;       // tells how far our target element is from the top of the page
-          var windowHeight = $(window).height();    // measures the window height
-          var buttonWidth = $this.parent().width(); // gets the width of our button
-          var buttonHeight = $this.parent().height();        // gets the height of our button
-          var windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
-          var currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
+          stickyTop = $this.offset().top;       // tells how far our target element is from the top of the page
+          windowHeight = $(window).height();    // measures the window height
+          buttonWidth = $this.parent().width(); // gets the width of our button
+          buttonHeight = $this.parent().height();        // gets the height of our button
+          windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
+          currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
 
           console.log(stickyTop);
           console.log(currentPosition - buttonHeight);
           console.log(stickyTop - (currentPosition - (buttonHeight)));
-
-          $this.css('position','static');  // this is to reset the position of the element whenever the page is updated with AJAX.
-          $this.width(buttonWidth);      // reset button width
 
           if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
             $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: $(this).parent().width() });   // stick it to the bottom
@@ -42,27 +56,24 @@
           else {
             $this.css({ position: 'static', width: buttonWidth });
           }
-    
-          $(window).scroll(function(){ // scroll event 
-            var windowTop = $(window).scrollTop(); // tells how far our screen is currently from the top of the page
-            var currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
-
-            console.log(stickyTop);
-            console.log(currentPosition - buttonHeight);
-            console.log(stickyTop - (currentPosition - (buttonHeight)));
-    
-            if (stickyTop > currentPosition) {    // if target element goes below the screen
-              $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: $this.parent().width() });    // stick it to the bottom
-            }
-            else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
-              $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: $this.parent().width() });   //stick it at the top
-            }
-            else {
-              $this.css({ position: 'static', width: buttonWidth });
-            }
-          });
         }); 
-      });         
+      });
+
+      $(window).scroll(function(){ // scroll event 
+        console.log(stickyTop);
+        console.log(currentPosition - buttonHeight);
+        console.log(stickyTop - (currentPosition - (buttonHeight)));
+
+        if (stickyTop > currentPosition) {    // if target element goes below the screen
+          $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: $this.parent().width() });    // stick it to the bottom
+        }
+        else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
+          $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: $this.parent().width() });   //stick it at the top
+        }
+        else {
+          $this.css({ position: 'static', width: buttonWidth });
+        }
+      });       
     }
   };
 }(jQuery));
