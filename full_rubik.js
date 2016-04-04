11382928c71256b28593e5362b92bcd6078f55f7
palagpatski <palagpatski@gmail.com>
2016-04-04 21:56:41 +0800

    Revert to commit be7b3e6b25624e8aa69410f589d688b450cb0ce8

--- full_rubik.js
+++ full_rubik.js
@@ -15,6 +15,17 @@
 
       // if ($('div[id*="edit-actions"]').length) {        // if element exists
 
+        function reset() {
+          $('div[id*="edit-actions"]').each(function (e) {
+            $this = $(this);
+            $this.width( $this.parent().width() );
+            $this.height( $this.parent.height() );
+            $this.css('position','static');
+          });
+        }
+        $(window).load(reset);
+        $(window).resize(reset);
+
         var stickyTop;
         var windowHeight;
         var buttonWidth;
@@ -65,16 +76,16 @@
         //   console.log(currentPosition - buttonHeight);
         //   console.log(stickyTop - (currentPosition - (buttonHeight)));
 
-        //   if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
-        //     $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: $(this).parent().width() });   // stick it to the bottom
-        //   }
-        //   else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
-        //     $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: $(this).parent().width() });   //stick it at the top
-        //   }
-        //   else {
-        //     $this.css({ position: 'static', width: buttonWidth });
-        //   }
-        // });
+          if (stickyTop > currentPosition) {    // if target element goes below the screen
+            $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: $this.parent().width() });    // stick it to the bottom
+          }
+          else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
+            $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: $this.parent().width() });   //stick it at the top
+          }
+          else {
+            $this.css({ position: 'static', width: buttonWidth });
+          }
+        });
       // }    
     }
   };
