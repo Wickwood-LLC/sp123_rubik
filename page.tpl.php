cb72eae6452781f2213e4ab708b3878c8ff66879
palagpatski <palagpatski@gmail.com>
2016-05-05 15:01:31 +0800

    Change login page and password request page titles.

--- page.tpl.php
+++ page.tpl.php
@@ -31,7 +31,13 @@
   <?php print render($title_prefix); ?>
   <h1 class='page-title <?php print $page_icon_class ?>'>
     <?php if (!empty($page_icon_class)): ?><span class='icon'></span><?php endif; ?>
-    <?php if ($title) print $title ?>
+    <?php if (arg(0) == 'user' && arg(1) == 'login') : ?>
+      <h1 class="page-title"> Log Into BWMA Control </h1>
+    <?php elseif (arg(0) == 'user' && arg(1) == 'password') : ?>
+      <h1 class="page-title"> Reset Your BWMA Control Password </h1>
+    <?php else : ?>
+      <?php if ($title) print $title ?>
+    <?php endif ; ?>
   </h1>
   <?php if ($action_links): ?>
     <ul class='action-links links clearfix'><?php print render($action_links) ?></ul>
