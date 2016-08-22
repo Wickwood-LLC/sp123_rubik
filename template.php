9b9b5ecf6d9f1b2d74e4269a0cdae4332c2e5fa8
palagpatski <palagpatski@gmail.com>
2016-08-22 16:14:21 +0800

    Remove custom code

--- template.php
+++ template.php
@@ -1,14 +1,1 @@
 <?php
-
-/**
- * Display a single views grouping.
- */
-function sp123_rubik_views_view_grouping($vars) {
-  $view = $vars['view'];
-  $title = $vars['title'];
-  $content = $vars['content'];
-
-  $output = theme('ctools_collapsible', array('handle' => $title, 'content' => $content, 'collapsed' => TRUE));
-
-  return $output;
-}
