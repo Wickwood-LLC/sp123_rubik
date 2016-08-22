20aa103b742fed8ffe811f143e96edce396edd91
palagpatski <palagpatski@gmail.com>
2016-08-22 16:08:36 +0800

    MAke views groups collpasible

--- template.php
+++ template.php
@@ -1,1 +1,14 @@
 <?php
+
+/**
+ * Display a single views grouping.
+ */
+function sp123_rubik_views_view_grouping($vars) {
+  $view = $vars['view'];
+  $title = $vars['title'];
+  $content = $vars['content'];
+
+  $output = theme('ctools_collapsible', array('handle' => $title, 'content' => $content, 'collapsed' => TRUE));
+
+  return $output;
+}
