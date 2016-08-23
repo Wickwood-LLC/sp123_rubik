095e56b0c917ef9493849d67695e5403229761b2
palagpatski <palagpatski@gmail.com>
2016-08-23 22:16:58 +0800

    Highlight for the active row.

--- min/sp123_rubik-min.js
+++ min/sp123_rubik-min.js
@@ -1,1 +1,1 @@
-!function($){Drupal.behaviors.accordionNavigation={attach:function(a,c){$(".ui-accordion a.active").parents(".ui-accordion-content").prev("h3.ui-accordion-header").addClass("accordion-header-active")}}}(jQuery);
+!function($){Drupal.behaviors.accordionNavigation={attach:function(a,c){$(".ui-accordion a.active").parents(".ui-accordion-content").prev("h3.ui-accordion-header").addClass("accordion-header-active"),$$(".ui-accordion a.active").parents(".views-row").addClass("active")}}}(jQuery);
--- sp123_rubik.js
+++ sp123_rubik.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.accordionNavigation = {
     attach: function (context, settings) {
 		$('.ui-accordion a.active').parents('.ui-accordion-content').prev('h3.ui-accordion-header').addClass('accordion-header-active');
+		$$('.ui-accordion a.active').parents('.views-row').addClass('active');
     }
   };
 }(jQuery));
