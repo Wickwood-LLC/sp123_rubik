c745d6b6a6d8500f79289c8f9b2135973f80d264
palagpatski <palagpatski@gmail.com>
2016-08-23 22:01:10 +0800

    Activate accordion group if child item is active.

--- sp123_rubik.js
+++ sp123_rubik.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.accordionNavigation = {
     attach: function (context, settings) {
-		$('.ui-accordion a.active').parents('h3.ui-accordion-header').addClass('accordion-header-active');
+		$('.ui-accordion a.active').parents('.ui-accordion-content').prev('h3.ui-accordion-header').addClass('accordion-header-active');
     }
   };
 }(jQuery));
