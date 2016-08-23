e93ecddbdb9cf8fc855b08051f6c7769349d955a
palagpatski <palagpatski@gmail.com>
2016-08-23 21:42:33 +0800

    Activate accordion group if child item is active.

--- sp123_rubik.js
+++ sp123_rubik.js
@@ -1,5 +1,10 @@
-$(function() {
-if ($('.views-accordion a.active').size()){
-$('.views-accordion:first div.accordion-content').hide().parent().find('h3').removeClass('accordion-header-active');
-$('.views-accordion a.active').parents('div.accordion-content').show().parents('div.views-accordion').find('h3').addClass('accordion-header-active');
-}});
+(function ($) {
+  Drupal.behaviors.accordionNavigation = {
+    attach: function (context, settings) {
+    	if ($('.views-accordion a.active').size()){
+			$('.views-accordion:first div.accordion-content').hide().parent().find('h3').removeClass('accordion-header-active');
+			$('.views-accordion a.active').parents('div.accordion-content').show().parents('div.views-accordion').find('h3').addClass('accordion-header-active');
+		}
+    }
+  };
+}(jQuery));
