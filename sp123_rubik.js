c9b19327e72bd5474d542b5608403766069a0d3c
palagpatski <palagpatski@gmail.com>
2016-08-23 22:26:47 +0800

    Highlight for the active row.

--- sp123_rubik.js
+++ sp123_rubik.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.accordionNavigation = {
     attach: function (context, settings) {
 		$('.ui-accordion a.active').parents('.ui-accordion-content').prev('h3.ui-accordion-header').addClass('accordion-header-active');
-		$('.ui-accordion a.active').parents('.views-row').addClass('active');
     }
   };
 }(jQuery));
