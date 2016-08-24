5f16b8854ce392812d2ca126561b6b8263a14424
palagpatski <palagpatski@gmail.com>
2016-08-24 14:17:46 +0800

    Toggle open the active group

--- sp123_rubik.js
+++ sp123_rubik.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.accordionNavigation = {
     attach: function (context, settings) {
 		$('.ui-accordion a.active').parents('.ui-accordion-content').addClass('ui-accordion-content-active').slideDown();
-		$('.ui-accordion a.active').parents('.ui-accordion-content').prev('h3.ui-accordion-header').addClass('ui-accordion-header-active ui-corner-top').removeClass('ui-corner-all');
+		$('.ui-accordion a.active').parents('.ui-accordion-content').prev('h3.ui-accordion-header').addClass('ui-accordion-header-active ui-state-active ui-corner-top').removeClass('ui-corner-all');
     }
   };
 }(jQuery));
