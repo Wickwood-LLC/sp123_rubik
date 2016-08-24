9e56a5b1e9f45a560c5e34f57f4f360e3d59aeed
palagpatski <palagpatski@gmail.com>
2016-08-24 14:10:26 +0800

    Toggle open the active group

--- min/sp123_rubik-min.js
+++ min/sp123_rubik-min.js
@@ -1,1 +1,1 @@
-!function($){Drupal.behaviors.accordionNavigation={attach:function(a,c){$(".ui-accordion a.active").parents(".ui-accordion-content").addClass("ui-accordion-content-active").slideDown()}}}(jQuery);
+!function($){Drupal.behaviors.accordionNavigation={attach:function(a,c){$(".ui-accordion a.active").parents(".ui-accordion-content").addClass("ui-accordion-content-active").slideDown(),$(".ui-accordion a.active").parents(".ui-accordion-content").prev("h3.ui-accordion-header").addClass("ui-accordion-header-active")}}}(jQuery);
