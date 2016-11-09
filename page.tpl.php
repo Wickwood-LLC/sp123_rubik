391a0037e58347e024521fe9fe0a7c636e3f33e4
palagpatski <palagpatski@gmail.com>
2016-11-09 16:09:14 +0800

    Improve template

--- page.tpl.php
+++ page.tpl.php
@@ -27,13 +27,7 @@
   <?php print render($title_prefix); ?>
   <h1 class='page-title <?php print $page_icon_class ?>'>
     <?php if (!empty($page_icon_class)): ?><span class='icon'></span><?php endif; ?>
-    <?php if (arg(0) == 'user' && arg(1) == 'login') : ?>
-      <h1 class="page-title"> Log In to <?php print strip_tags($site_name); ?> </h1>
-    <?php elseif (arg(0) == 'user' && arg(1) == 'password') : ?>
-      <h1 class="page-title"> Reset Your <?php print strip_tags($site_name); ?> Password </h1>
-    <?php else : ?>
-      <?php if ($title) print $title ?>
-    <?php endif ; ?>
+    <?php if ($title) print $title ?>
   </h1>
   <?php if ($action_links): ?>
     <ul class='action-links links clearfix'><?php print render($action_links) ?></ul>
