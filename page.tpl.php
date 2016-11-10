bb6e390d167b2f5ea5f7315a55412e16ec1d303d
palagpatski <palagpatski@gmail.com>
2016-11-10 13:55:11 +0800

    Add branding

--- page.tpl.php
+++ page.tpl.php
@@ -11,3 +11,16 @@
   <?php print render($page['header']); ?>
   <?php print render($page['navbar']); ?>
 </header>
+<?php if (theme_get_setting('rubik_show_branding')): ?>
+  <div id='branding'><div class='limiter clearfix'>  
+    <?php if ((arg(0) == 'user' && arg(1) == 'login') || (arg(0) == 'user' && arg(1) == 'password')): ?>
+      <div class='breadcrumb clearfix'></div>
+    <?php else: ?>
+      <div class='breadcrumb clearfix'><?php print $breadcrumb ?></div>
+    <?php endif; ?>
+  
+    <?php if (!$overlay && isset($secondary_menu)) : ?>
+      <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('class' => 'links secondary-menu'))) ?>
+    <?php endif; ?>
+  </div></div>
+<?php endif; ?>
