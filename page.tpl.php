4c837190b89595cf414c4cee44ba96d45993d083
palagpatski <palagpatski@gmail.com>
2016-11-10 14:16:17 +0800

    Add footer again with a modified markup

--- page.tpl.php
+++ page.tpl.php
@@ -58,3 +58,7 @@
     <?php if (!empty($page['content'])) print render($page['content']) ?>
   </div>
 </div></div>
+
+<div id='footer' class='clearfix'>
+  <?php if (!empty($page['footer'])) print render($page['footer']) ?>
+</div>
