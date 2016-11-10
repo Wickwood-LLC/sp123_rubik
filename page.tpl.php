d7c50ae3839d710d2a70e55b30f2b9ff55504e7d
palagpatski <palagpatski@gmail.com>
2016-11-10 14:04:53 +0800

    Adding the main page content

--- page.tpl.php
+++ page.tpl.php
@@ -52,3 +52,9 @@
 <div id='console'><div class='limiter clearfix'><?php print $messages; ?></div></div>
 <?php endif; ?>
 
+<div id='page'><div id='main-content' class='limiter clearfix'>
+  <?php if ($page['help']) print render($page['help']) ?>
+  <div id='content' class='page-content clearfix'>
+    <?php if (!empty($page['content'])) print render($page['content']) ?>
+  </div>
+</div></div>
