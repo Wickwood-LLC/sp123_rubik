3d347582103a873fdbf0802fb572c9449a9b073a
palagpatski <palagpatski@gmail.com>
2016-11-10 15:07:46 +0800

    Modified footer

--- page.tpl.php
+++ page.tpl.php
@@ -51,12 +51,5 @@
 </div></div>
 
 <div id='footer' class='clearfix'>
-  <?php if ($feed_icons): ?>
-    <div class='feed-icons clearfix'>
-      <label><?php print t('Feeds') ?></label>
-      <?php print $feed_icons ?>
-    </div>
-  <?php endif; ?>
+  <?php print render($page['footer']); ?>
 </div>
-
-<?php print render($page['footer']); ?>
