ff82c9f8b396555658542f7ca82ce8554a829ced
palagpatski <palagpatski@gmail.com>
2016-11-10 14:46:52 +0800

    Add footer

--- page.tpl.php
+++ page.tpl.php
@@ -50,4 +50,13 @@
   </div>
 </div></div>
 
+<div id='footer' class='clearfix'>
+  <?php if ($feed_icons): ?>
+    <div class='feed-icons clearfix'>
+      <label><?php print t('Feeds') ?></label>
+      <?php print $feed_icons ?>
+    </div>
+  <?php endif; ?>
+</div>
 
+<?php print render($page['footer']); ?>
