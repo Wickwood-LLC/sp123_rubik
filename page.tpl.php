a2e01c7ed8d72bfbf3997e37ad358b6290ec65ef
palagpatski <palagpatski@gmail.com>
2016-11-10 13:58:10 +0800

    Add page title

--- page.tpl.php
+++ page.tpl.php
@@ -24,3 +24,26 @@
     <?php endif; ?>
   </div></div>
 <?php endif; ?>
+
+<div id='page-title'><div class='limiter clearfix'>
+  <div class='tabs clearfix'>
+    <?php if ($primary_local_tasks): ?>
+      <ul class='primary-tabs links clearfix'><?php print render($primary_local_tasks) ?></ul>
+    <?php endif; ?>
+  </div>
+  <?php print render($title_prefix); ?>
+  <h1 class='page-title <?php print $page_icon_class ?>'>
+    <?php if (!empty($page_icon_class)): ?><span class='icon'></span><?php endif; ?>
+    <?php if (arg(0) == 'user' && arg(1) == 'login') : ?>
+      <h1 class="page-title"> Log In to <?php print strip_tags($site_name); ?> </h1>
+    <?php elseif (arg(0) == 'user' && arg(1) == 'password') : ?>
+      <h1 class="page-title"> Reset Your <?php print strip_tags($site_name); ?> Password </h1>
+    <?php else : ?>
+      <?php if ($title) print $title ?>
+    <?php endif ; ?>
+  </h1>
+  <?php if ($action_links): ?>
+    <ul class='action-links links clearfix'><?php print render($action_links) ?></ul>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+</div></div>
